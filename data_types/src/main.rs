654820adf7aca2af4b3075e177a6912f598a49fe
eric-k-chu <kiyuen88ec@gmail.com>
2024-03-17 04:04:06 -0700

    u16 addition

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,11 @@
+fn add(a: &u16, b: &u16) -> u16 {
+    return a + b;
+}
+
 fn main() {
-    println!("Hello World!")
+    let a: u16 = 5;
+    let b: u16 = 4;
+    let sum: u16 = add(&a, &b);
+
+    println!("{} + {} = {}", a, b, sum);
 }
